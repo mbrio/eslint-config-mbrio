27764a04142750e5c6274cd4973fba493257fddd
Michael Diolosa <michael.diolosa@gmail.com>
2016-08-15 15:26:07 -0400

    Initial build

---
+++ README.md
@@ -0,0 +1,5 @@
+# eslint-config-mbrio
+
+There are two sets of ESLint rules in this repository, one to be used in the
+browser for React development and one for use in Node 6 applications that
+support ES6 standards.
---
+++ browser.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  'extends': [
+    './config/babel.js',
+    './config/node.js',
+    './config/browser.js',
+    './config/errors.js',
+    './config/strict.js',
+    './config/variables.js',
+    './config/best-practices.js',
+    './config/es6.js',
+    './config/style.js',
+    './config/react.js',
+  ],
+};
